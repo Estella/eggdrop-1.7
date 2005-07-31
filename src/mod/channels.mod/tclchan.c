f10e3d8719e43789ab2612d2a7755a71b5dab9a6
wcc <wcc>
2005-07-31 06:19:57 +0000

    * Fixed bug 412, found by Chris Northwood.

--- src/mod/channels.mod/tclchan.c
+++ src/mod/channels.mod/tclchan.c
@@ -1,7 +1,7 @@
 /*
  * tclchan.c -- part of channels.mod
  *
- * $Id: tclchan.c,v 1.2 2005/01/21 01:43:41 wcc Exp $
+ * $Id: tclchan.c,v 1.3 2005/07/31 06:19:57 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -955,7 +955,7 @@ static int tcl_channel_get(Tcl_Interp *irp, struct chanset_t *chan,
     s[120] = 0;
   } else if (!strcmp(setting, "idle-kick"))
     simple_sprintf(s, "%d", chan->idle_kick);
-  else if (!strcmp(setting, "stop-net-hack"))
+  else if (!strcmp(setting, "stopnethack-mode"))
     simple_sprintf(s, "%d", chan->stopnethack_mode);
   else if (!strcmp(setting, "revenge-mode"))
     simple_sprintf(s, "%d", chan->revenge_mode);
--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.37 2005/07/31 06:00:25 wcc Exp $
+ * $Id: patch.h,v 1.38 2005/07/31 06:19:57 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1122789598");            /* current unixtime */
+patch("1122790763");            /* current unixtime */
 /*
  *
  *
  */
-patch("IT-COMPILES!");
+patch("stopnethack-get");
 /*
  *
  *
